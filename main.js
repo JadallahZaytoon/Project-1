f2c7b09431796c73262a1982b192d1fd3e8f2baa
JadallahZaytoon <jadallah.zaytoon@gmail.com>
2021-04-23 22:44:45 +0300

    in progress maintaining pages functinality

---
+++ main.js
@@ -0,0 +1,23 @@
+
+
+ getName=()=>{
+    var userN =document.getElementById("player-name-input").value;  
+    console.log(userN);
+    sessionStorage.setItem("user", userN);
+};
+
+
+// sessionStorage.setItem("favoriteMovie", favoritemovie);
+// const headerr=document.querySelector("h1")
+// headerr.id="myH1";
+// headerr.innerText=inputValue
+
+
+
+
+
+   
+   
+
+
+
---
+++ play.js
@@ -0,0 +1,13 @@
+// var userN = sessionStorage.getItem("user");
+// console.log(userN);
+
+// const h11=document.querySelector("h1");
+// h11.id="heas"
+// h11.innerText=userN;
+// const myPageBody=document.querySelector("body")
+// myPageBody.append(h11)
+
+
+// let counter = document.querySelector("div");
+// counter.id="counter";
+// counter.innerText="timer";
